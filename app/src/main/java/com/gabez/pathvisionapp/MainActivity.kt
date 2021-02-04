1abb8aed7b53d87cc4f457e74f63dfa6a7905c99
GabezRules <gabriela.zofia.zawadzka@gmail.com>
2021-02-04 16:20:26 +0100

    tab layout

--- app/src/main/java/com/gabez/pathvisionapp/MainActivity.kt
+++ app/src/main/java/com/gabez/pathvisionapp/MainActivity.kt
@@ -2,10 +2,49 @@ package com.gabez.pathvisionapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+import androidx.viewpager.widget.ViewPager
+import com.iammert.library.AnimatedTabLayout
 
-class MainActivity : AppCompatActivity() {
+const val NUM_PAGES = 2
+
+class MainActivity : FragmentActivity() {
+
+    private lateinit var viewPager: ViewPager
+    private lateinit var bottomNav: AnimatedTabLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        viewPager = findViewById(R.id.viewPager)
+        bottomNav = findViewById(R.id.bottomNav)
+
+        bottomNav.setupViewPager(viewPager)
+        bottomNav.setTabChangeListener(object : AnimatedTabLayout.OnChangeListener {
+            override fun onChanged(position: Int) {
+            }
+        })
+
+        val pagerAdapter = ScreenSlidePagerAdapter(supportFragmentManager)
+        viewPager.adapter = pagerAdapter
+        viewPager.setPageTransformer(true, DepthPageTransformer())
+
     }
+
+    private inner class ScreenSlidePagerAdapter(fm: FragmentManager) : FragmentStatePagerAdapter(fm) {
+
+        override fun getCount(): Int = NUM_PAGES
+
+        override fun getItem(position: Int): Fragment = ScreenSlidePageFragment()
+    }
+
+    override fun onBackPressed() {
+        if (viewPager.currentItem == 0) super.onBackPressed()
+        else viewPager.currentItem = viewPager.currentItem - 1
+    }
+
+
 }

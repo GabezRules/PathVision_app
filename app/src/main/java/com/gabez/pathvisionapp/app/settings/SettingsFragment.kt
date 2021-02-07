68dd8352ddd1ff31291a4aceefdf3ff6a913020e
GabezRules <gabriela.zofia.zawadzka@gmail.com>
2021-02-07 23:12:53 +0100

    show authentication dialogs

--- app/src/main/java/com/gabez/pathvisionapp/app/settings/SettingsFragment.kt
+++ app/src/main/java/com/gabez/pathvisionapp/app/settings/SettingsFragment.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.gabez.pathvisionapp.R
+import com.gabez.pathvisionapp.app.settings.authentication.LoginBottomsheetFragment
+import com.gabez.pathvisionapp.app.settings.authentication.RegisterBottomsheetFragment
 import com.google.android.material.button.MaterialButton
 import com.greenfrvr.hashtagview.HashtagView
 
@@ -15,7 +17,14 @@ class SettingsFragment : Fragment() {
     private lateinit var buttonLogin: MaterialButton
     private lateinit var buttonRegister: MaterialButton
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    val LOGIN_FRAGMENT_TAG = "com.gabez.pathvisionapp.app.settings.LOGIN_FRAGMENT"
+    val REGISTER_FRAGMENT_TAG = "com.gabez.pathvisionapp.app.settings.REGISTER_FRAGMENT"
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         val view = inflater.inflate(R.layout.fragment_settings, container, false)
 
         buttonLogin = view.findViewById(R.id.buttonLogin)
@@ -27,12 +36,16 @@ class SettingsFragment : Fragment() {
         return view
     }
 
-    fun showLoginDialog(){
-
+    fun showLoginDialog() {
+        requireActivity().supportFragmentManager.let {
+            LoginBottomsheetFragment.newInstance().apply { show(it, LOGIN_FRAGMENT_TAG) }
+        }
     }
 
-    fun showRegisterDialog(){
-
+    fun showRegisterDialog() {
+        requireActivity().supportFragmentManager.let {
+            RegisterBottomsheetFragment.newInstance().apply { show(it, REGISTER_FRAGMENT_TAG) }
+        }
     }
 
     companion object {
